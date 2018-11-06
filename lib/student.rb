cda3c379c8b50ff28607e122ea286cc993a5bea2
Daniel Etter <etter.dt@gmail.com>
2018-11-06 15:09:37 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -98,8 +98,10 @@ class Student
       WHERE grade = 10
       LIMIT 1
       SQL
-    student_array = DB[:conn].execute(sql)
-      
+    first_student_array = DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+    first_student.id = 
   end
   
   def self.all_students_in_grade_X(numb)
